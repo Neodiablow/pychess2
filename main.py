8ac56ed6d073ada895c07b48ba3600cb009437cf
Immunia <levpoubelle1@gmail.com>
2016-01-19 14:39:16 +0100

    Board
    Basic game board

---
+++ main.py
@@ -0,0 +1,21 @@
+from IHM.mainIHM import MainIHMBoard,moveIHM,setInfosPiecesIHM
+
+
+def callbackMove(x1, y1, x2, y2):
+    global mainIHMBoard
+    moveIHM(x1, y1, x2, y2)
+    
+def callbackInfosPieces(x1, y1):
+    listPossibleMoves = []
+    #== Pour tester ==
+    listPossibleMoves.append([x1+1,y1])
+    listPossibleMoves.append([x1,y1+1])
+    listPossibleMoves.append([x1-1,y1])
+    listPossibleMoves.append([x1,y1-1])
+    #== Fin pour tester
+    setInfosPiecesIHM(x1, y1, listPossibleMoves)
+    
+mainIHMBoard = MainIHMBoard("normal", "animaux", callbackMove, callbackInfosPieces)
+
+while True:
+    input("Console: ")
