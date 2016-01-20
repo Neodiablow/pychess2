cf9491afa328e4037e2e12dd01c796fb5b8500a8
Neodiablow <francedupont6@hotmail.com>
2016-01-21 00:21:00 +0100

    clean merge

--- IHM/mainIHM.py
+++ IHM/mainIHM.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 from tkinter import * 
 from threading import *
 import time
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 from IHM.mainIHM import MainIHMBoard,moveIHM,setInfosPiecesIHM
 
 
