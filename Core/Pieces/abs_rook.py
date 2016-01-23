0c65b1c677d05290fa7d2e7a89aca9b2e1c00113
Neodiablow <francedupont6@hotmail.com>
2016-01-23 15:04:41 +0100

    add rook

---
+++ Core/Pieces/abs_rook.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+import string
+alphabet=string.ascii_uppercase[0:8] #string used for chess board
+lb = alphabet.index("B")
+ub = alphabet.index("G")
+
+class AbsRook:
+    """A piece of chess"""
+    def __init__(self,c,a):
+        self.piece="Rook"
+        self.color=bool(c)
+        self.army=str(a)
+    
+    def rookMoves(self,x,y):
+    #Returns Valid moves to the model
+        listV=[]
+        listH=[]
+        """Rook moves, vertical line, + horizontal line
+        add the tuple to lists(H&V) declared above"""
+        for yValue in range(1,9):
+           listV+=[(x,yValue)] 
+        for xValue in range(alphabet.index('A'),alphabet.index('H')+1):
+           listH+=[(alphabet[xValue],y)]
+
+        """Remove moves which are not moves"""
+        listH.remove((x,y))
+        listV.remove((x,y))
+
+
+        return listH+listV
+
