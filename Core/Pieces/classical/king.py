3285603759b41a149f72e650367fe7503c3a23bb
Neodiablow <francedupont6@hotmail.com>
2016-02-02 00:11:42 +0100

    new file organisation

---
+++ Core/Pieces/classical/king.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+import string
+from Core.Pieces.abs_king import AbsKing
+alphabet=string.ascii_uppercase[0:8] #string used for chess board
+lb = alphabet.index("B")
+ub = alphabet.index("G")
+
+class King(AbsKing):
+    """Most important piece of chess"""
+    def __init__(self,c,a):
+        self.piece="King"
+        self.color=bool(c)
+        self.army=str(a)
+    
+    def moves(self,x,y):
+        return self.kingMoves(x,y)
+#        """Returns Valid moves to the model"""
+#        listK=[]
+#        if y<8:                                             #if y<8
+#            listK+=[(x,y+1)]                                #can move upword
+#        if alphabet.index("H")>alphabet.index(x):           #if x<H
+#            listK+=[(alphabet[alphabet.index(x)+1],y)]      #can move on the right
+#        if y>1:                                             #if y>1
+#            listK+=[(x,y-1)]                                #can move downward
+#        if alphabet.index("A")<alphabet.index(x):           #if x> A
+#            listK+=[(alphabet[alphabet.index(x)-1],y)]      #can move on the left
+#        if y<8 and alphabet.index("H")>alphabet.index(x):   #if y<8 && x<y 
+#            listK+=[(alphabet[alphabet.index(x)+1],y+1)]    #can move up right
+#        if y<8 and alphabet.index("A")<alphabet.index(x):   #etc
+#            listK+=[(alphabet[alphabet.index(x)-1],y+1)]    #up left
+#        if y>1 and alphabet.index("H")>alphabet.index(x):   #etc
+#            listK+=[(alphabet[alphabet.index(x)+1],y-1)]    #down right
+#        if y>1 and alphabet.index("A")<alphabet.index(x):   #etc
+#            listK+=[(alphabet[alphabet.index(x)-1],y-1)]    #down left
+#            
+#        return listK
+    
+    def smallCastle(self):
+        if self.color and self.army == "Classic":
+            return ["G",1]
+        elif self.army == "Classic":
+            return ["G",8]
+        else :
+            return []
+    def bigCastle(self):
+        if self.color and self.army == "Classic":
+            return ["B",1]
+        elif self.army == "Classic":
+            return ["B",8]
+        else :
+            return []
+
+king=King(True,"Classical");
+print(king.moves("C",4))
+
