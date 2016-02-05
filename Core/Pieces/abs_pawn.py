b477bb57df0646ccbc8124d9c21eec5d9eeaf10e
Neodiablow <francedupont6@hotmail.com>
2016-02-05 20:36:06 +0100

    debug pawn

--- Core/Pieces/abs_pawn.py
+++ Core/Pieces/abs_pawn.py
@@ -14,23 +14,25 @@ class AbsPawn:
     def pawnMoves(self,x,y):
         """Put moves in listP"""
         listP=[]
-        if alphabet.index(x) in range(lb,ub):
-            if self.color:
-                listP+= [(alphabet[alphabet.index(x)],y+1),(alphabet[alphabet.index(x)],y+2),(alphabet[alphabet.index(x)+1],y+1),(alphabet[alphabet.index(x)-1],y+1)]
-            else:
-                listP+= [(alphabet[alphabet.index(x)],y-1),(alphabet[alphabet.index(x)],y-2),(alphabet[alphabet.index(x)+1],y-1),(alphabet[alphabet.index(x)-1],y-1)]
-        elif alphabet.index(x) == alphabet.index("A"):
-            if self.color:
-                listP+= [(alphabet[alphabet.index(x)],y+1),(alphabet[alphabet.index(x)],y+2),(alphabet[alphabet.index(x)+1],y+1)]
-            else:
-                listP+= [(alphabet[alphabet.index(x)],y-1),(alphabet[alphabet.index(x)],y-2),(alphabet[alphabet.index(x)+1],y-1)]
-            
-        elif alphabet.index(x) == alphabet.index("H"):
-            if self.color:
-                listP+= [(alphabet[alphabet.index(x)],y+1),(alphabet[alphabet.index(x)],y+2),(alphabet[alphabet.index(x)-1],y+1)]
-            else:
-                listP+= [(alphabet[alphabet.index(x)],y-1),(alphabet[alphabet.index(x)],y-2),(alphabet[alphabet.index(x)-1],y-1)]
+        if self.color:
+            listP+=[(alphabet[alphabet.index(x)],y+1)]
+            listP+=[(alphabet[alphabet.index(x)],y+2)]
 
+            if alphabet.index("H")>=alphabet.index(x)+1 and y<8:
+                listP+=[(alphabet[alphabet.index(x)+1],y+1)]
+
+            if alphabet.index("A")<=alphabet.index(x)-1 and y<8:
+                listP+=[(alphabet[alphabet.index(x)-1],y+1)]
+        
+        else:
+            listP+=[(alphabet[alphabet.index(x)],y-1)]
+            listP+=[(alphabet[alphabet.index(x)],y-2)]
+
+            if alphabet.index("H")>=alphabet.index(x)+1 and y>1:
+                listP+=[(alphabet[alphabet.index(x)+1],y-1)]
+
+            if alphabet.index("A")<=alphabet.index(x)-1 and y>1:
+                listP+=[(alphabet[alphabet.index(x)-1],y-1)]
         """Remove uncorrect moves from listP"""
         listR=[]
         for i in listP:
