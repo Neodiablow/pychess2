c3593221e890f049ad55759014ba3b74c26e78ef
Neodiablow <francedupont6@hotmail.com>
2016-02-06 14:39:38 +0100

    add all animals

---
+++ Core/Pieces/animals/wildhorse.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+import string
+from Core.Pieces.abs_knight import AbsKnight
+alphabet=string.ascii_uppercase[0:8] #string used for chess board
+lb = alphabet.index("B")
+ub = alphabet.index("G")
+
+class WildHorse(AbsKnight):
+    def moves(self,x,y):
+        return self.knightMoves(x,y)
